bdad2d8671fa1af6d6e0aab9d39069f57b4d6c37
Jered D. Leisey <jered.leisey@gmail.com>
2020-10-19 11:31:35 -0600

    Directory structure and MVC files created

---
+++ Converse/Controllers/VideoPlayerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VideoPlayerViewController.swift
+//  Converse
+//
+//  Created by Jered Leisey on 10/12/20.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
