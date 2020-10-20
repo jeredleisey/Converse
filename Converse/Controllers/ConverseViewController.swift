7bb672c4e7aab646776ceed78aef833fde962de9
Jered D. Leisey <jered.leisey@gmail.com>
2020-10-20 11:56:35 -0600

    Firebase database configured, login persistence implemented for basic email.

--- Converse/Controllers/ConverseViewController.swift
+++ Converse/Controllers/ConverseViewController.swift
@@ -6,21 +6,24 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ConverseViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 		view.backgroundColor = .red
+		
 	}
 	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
 		
-		let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
-		
-		if !isLoggedIn {
+		validateAuth()
+	}
+	
+	private func validateAuth() {
+		if FirebaseAuth.Auth.auth().currentUser == nil {
 			let vc = LoginViewController()
 			let nav = UINavigationController(rootViewController: vc)
 			nav.modalPresentationStyle = .fullScreen
